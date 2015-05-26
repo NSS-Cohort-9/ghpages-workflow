b3b09939c5d988ba74cfa5ffdd9adc07a37da0e1
Scott Humphries <scott@sscotth.io>
2015-05-26 11:32:48 -0500

    babel, jade, and sass grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,24 @@
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    babel: {
+      main: {
+        options: {
+          sourceMap: 'inline'
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: ['**/*.js'],
+            dest: 'public/'
+          }
+        ]
+      }
+    },
     clean: ['public'],
     copy: {
       main: {
@@ -16,6 +30,33 @@ module.exports = function(grunt) {
             dest: 'public/'
           }
         ]
+      }
+    },
+    jade: {
+      main: {
+        options: {
+          pretty: true
+        },
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: ['**/*.jade', '!**/_*.jade'],
+            dest: 'public/',
+            ext: '.html'
+          }
+        ]
+      }
+    },
+    sass: {
+      main: {
+        options: {
+          sourceMap: true,
+          sourceMapEmbed: true
+        },
+        files: {
+          'public/css/main.css': 'src/_styles/main.scss'
+        }
       }
     }
   });
